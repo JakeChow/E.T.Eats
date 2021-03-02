af2392dda27720d22fa64a9970a8c4171f2b78c1
icyoutoo <66316658+icyoutoo@users.noreply.github.com>
2021-03-01 19:34:42 -0500

    LevelManager
    LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public Text gameText;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LevelBeat()
+    {
+        gameText.text = "YOU WIN!";
+        gameText.gameObject.SetActive(true);
+    }
+}
