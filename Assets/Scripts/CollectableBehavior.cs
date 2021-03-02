54ddcff187f3ff5a824147e77d1218343c255536
Jake Mayeux <jakemayeux@gmail.com>
2021-03-01 19:17:47 -0500

    add collectables

---
+++ Assets/Scripts/CollectableBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableBehavior : MonoBehaviour
+{
+
+    public static int collected = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Player") {
+
+            CollectableBehavior.collected ++;
+
+            UpdateUI();
+
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateUI() {
+        scoreText.text = "Score: " + collected;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,64 +3,62 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-		private Animator anim;
-		private CharacterController controller;
+	private Animator anim;
+	private CharacterController controller;
 
-		public float moveSpeed = 50.0f;
-		public float turnSpeed = 400.0f;
-		public float gravity = 20.0f;
-        public float airControl = 10;
-        public float jumpHeight = 5;
+	public float moveSpeed = 50.0f;
+	public float turnSpeed = 400.0f;
+	public float gravity = 20.0f;
+	public float airControl = 10;
+	public float jumpHeight = 5;
 
-    Vector3 input, moveDirection;
+	Vector3 input, moveDirection;
 
-    void Start () {
-			controller = GetComponent <CharacterController>();
-			anim = gameObject.GetComponentInChildren<Animator>();
+	void Start () {
+		controller = GetComponent <CharacterController>();
+		anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	void Update() {
+		if (Input.GetKey("w"))
+		{
+			anim.SetInteger("AnimationPar", 1);
+		}
+		else
+		{
+			anim.SetInteger("AnimationPar", 0);
 		}
 
-    void Update()
-    {
-        if (Input.GetKey("w"))
-        {
-            anim.SetInteger("AnimationPar", 1);
-        }
-        else
-        {
-            anim.SetInteger("AnimationPar", 0);
-        }
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+		input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
 
-        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+		input *= moveSpeed;
 
-        input *= moveSpeed;
+		if (controller.isGrounded)
+		{
 
-        if (controller.isGrounded)
-        {
+			moveDirection = input;
 
-            moveDirection = input;
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+			}
+			else
+			{
+				moveDirection.y = 0.0f;
+			}
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-            }
-            else
-            {
-                moveDirection.y = 0.0f;
-            }
+		}
+		else
+		{
+			//we are midair
+			input.y = moveDirection.y;
+			moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+		}
 
-        }
-        else
-        {
-            //we are midair
-            input.y = moveDirection.y;
-            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
-        }
-
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
-    }
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
 }
-
