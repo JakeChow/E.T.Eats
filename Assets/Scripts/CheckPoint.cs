2a6d6210259c344bb6591e2a7cbb90ac3a6fee19
JakeChow <56766171+JakeChow@users.noreply.github.com>
2021-04-05 14:48:42 -0400

    checkpoints (#17)

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    
+    private GameObject player;
+
+    private bool check = false;
+    private Transform checkpointLocation;
+
+    public GameObject CheckpointChecked;
+    public GameObject CheckpointUnchecked;
+
+    
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        checkpointLocation = CheckpointChecked.transform;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            CheckpointChecked.SetActive(true);
+            CheckpointUnchecked.SetActive(false);
+            check = true;
+        }
+    }
+
+    public void JumpToCheckpoint()
+    {
+        if (check)
+        {
+            player.transform.position = checkpointLocation.position;
+        }
+    }
+}
