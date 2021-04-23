7192efc24174c007c288401784fb44a7f6544fec
icyoutoo <66316658+icyoutoo@users.noreply.github.com>
2021-04-23 16:22:19 -0400

    Added spring animation and sound effect

---
+++ Assets/Scripts/SpringBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBehavior : MonoBehaviour
+{
+
+    
+    public AudioClip springSFX;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine("PlayAnim");
+            AudioSource.PlayClipAtPoint(springSFX, transform.position);
+            
+        }
+    }
+    IEnumerator PlayAnim()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("springActivate");
+        yield return new WaitForSeconds(2);
+        gameObject.GetComponent<Animator>().ResetTrigger("springActivate");
+    }
+}
